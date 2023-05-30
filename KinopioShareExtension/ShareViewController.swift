51e45453da8510584cf310f0ff35d668074c5afa
Lucas Fischer <lucas.fischer@me.com>
2023-05-30 20:03:43 +0200

    add `navigator.isSecureAppContext = true` to share extension

--- KinopioShareExtension/ShareViewController.swift
+++ KinopioShareExtension/ShareViewController.swift
@@ -104,7 +104,7 @@ extension ShareViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         
         let escapedText = text.replacingOccurrences(of: "'", with: "\'")
-        let script = "window.postMessage('\(escapedText)', '*')"
+        let script = "window.postMessage('\(escapedText)', '*');navigator.isSecureAppContext = true;"
         
         webView.evaluateJavaScript(script)
     }
