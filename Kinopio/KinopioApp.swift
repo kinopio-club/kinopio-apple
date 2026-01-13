365ba34cfd9cb5b8d166c38b6ea033ce9a2c6c6b
Lucas Fischer <lucas.fischer@me.com>
2026-01-13 09:26:54 +0800

    index spaces and cache space thumbnails independent of each other

--- Kinopio/KinopioApp.swift
+++ Kinopio/KinopioApp.swift
@@ -7,36 +7,49 @@ struct KinopioApp: App {
     
     @Environment(\.scenePhase) private var scenePhase
     
+    func indexSpaces() async {
+        let date = Date().timeIntervalSinceReferenceDate
+        
+        guard let token = Storage.getToken(), #available(iOS 18.0, *) else {
+            return
+        }
+        do {
+            // MARK: - Index Spaces
+            let spaces = try await Networking.getUserSpaces(token: token)
+            var spaceEntities = [SpaceEntity]()
+            
+            for space in spaces {
+                spaceEntities.append(
+                    SpaceEntity(
+                        id: space.id,
+                        name: space.name,
+                        thumbnailURL: await ThumbnailCache.shared.cachedImageURL(for: space),
+                        editedAt: space.editedAt,
+                        createdAt: space.createdAt
+                    )
+                )
+            }
+            try await CSSearchableIndex.default().indexAppEntities(spaceEntities)
+            
+            // MARK: - Warmup Thumbnail Caches
+            await withTaskGroup { group in
+                for space in spaces.prefix(30) {
+                    group.addTask {
+                        let _ = try? await ThumbnailCache.shared.imageURL(for: space)
+                    }
+                }
+                await group.waitForAll()
+            }
+        } catch {
+            print(error)
+        }
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .task {
-                    guard let token = Storage.getToken(), #available(iOS 18.0, *) else {
-                        return
-                    }
-                    do {
-                        let spaces = try await Networking.getUserSpaces(token: token)
-                        
-                        var spaceEntities = [SpaceEntity]()
-                        for space in spaces {
-                            spaceEntities.append(
-                                SpaceEntity(
-                                    id: space.id,
-                                    name: space.name,
-                                    thumbnailURL: try? await ThumbnailCache.shared.imageURL(for: space),
-                                    editedAt: space.editedAt,
-                                    createdAt: space.createdAt
-                                )
-                            )
-                        }
-                        
-                        try await CSSearchableIndex.default().indexAppEntities(
-                            spaceEntities
-                        )
-                    } catch {
-                        print(error)
-                    }
-                }
+                .task { await indexSpaces() }
         }
         .onChange(of: scenePhase) { scenePhase in
             if scenePhase == .background {
